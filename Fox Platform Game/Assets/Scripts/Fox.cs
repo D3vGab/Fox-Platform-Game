171a8a7b6ea163f150c205a60b194e4fcb47cbf8
D3vGab <gabed3v@gmail.com>
2022-12-18 01:28:18 -0300

    Movement
    Adding movement to player

---
+++ Fox Platform Game/Assets/Scripts/Fox.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rg;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rg = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+
+        rg.velocity = new Vector2(inputX*speed, 0);
+
+        if(inputX > 0)
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0,0,0);
+        }else if (inputX < 0) 
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0,180,0);
+        }
+        else 
+        {
+            anim.SetBool("walk", false);
+        }
+    }
+}
